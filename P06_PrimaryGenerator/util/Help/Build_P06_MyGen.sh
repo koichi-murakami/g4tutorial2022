7e2a6d393fe1859d31450dcb49adb23713b7788d
Koichi Murakami <koichi.murakami@kek.jp>
2022-11-15 22:25:14 +0900

    add P06

---
+++ P06_PrimaryGenerator/util/Help/Build_P06_MyGen.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#########################################################################
+# This is a script to build a tutorial application
+#########################################################################
+
+### Set variables
+assumedDir="P06_PrimaryGenerator"
+nameFileSource="./util/G4Codes/PrimaryGenerator.cc_MyGen"
+nameFileHeader="./util/G4Codes/PrimaryGenerator.hh_MyGen"
+
+### Check whether in the right directory
+currentDir=`basename $(pwd)`
+if [ $currentDir != $assumedDir ] ; then
+  echo "Error: The script is only allowed to execute under '$assumedDir'"
+  exit 1
+fi
+
+### Copy the source/header files to the target
+if [ -f $nameFileSource ] ; then
+  cp $nameFileSource ./source/src/PrimaryGenerator.cc
+else
+  echo "Error: The source file '$nameFileSource' does not exist!"
+  exit 1
+fi
+
+if [ -f $nameFileHeader ] ; then
+  cp $nameFileHeader ./source/include/PrimaryGenerator.hh
+else
+  echo "Error: The source file '$nameFileHeader' does not exist!"
+  exit 1
+fi
+
+
+### Clean up work directories
+if [ -d build ] ; then
+  rm -r build
+fi
+
+if [ -d bin ] ;then
+  rm -r bin
+fi
+
+### Build using cmake
+mkdir build
+cd build
+cmake ../source
+make
+make install
---
+++ P06_PrimaryGenerator/util/Help/Build_P06_MyGen_MagField.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+#########################################################################
+# This is a script to build a tutorial application
+#########################################################################
+
+### Set variables
+assumedDir="P06_PrimaryGenerator"
+nameFileSource="./util/G4Codes/PrimaryGenerator.cc_MyGen"
+nameFileHeader="./util/G4Codes/PrimaryGenerator.hh_MyGen"
+nameFileGeom="./util/G4Codes/Geometry.cc_MagField"
+
+### Check whether in the right directory
+currentDir=`basename $(pwd)`
+if [ $currentDir != $assumedDir ] ; then
+  echo "Error: The script is only allowed to execute under '$assumedDir'"
+  exit 1
+fi
+
+### Copy the source/header files to the target
+if [ -f $nameFileSource ] ; then
+  cp $nameFileSource ./source/src/PrimaryGenerator.cc
+else
+  echo "Error: The source file '$nameFileSource' does not exist!"
+  exit 1
+fi
+
+if [ -f $nameFileHeader ] ; then
+  cp $nameFileHeader ./source/include/PrimaryGenerator.hh
+else
+  echo "Error: The source file '$nameFileHeader' does not exist!"
+  exit 1
+fi
+
+if [ -f $nameFileGeom ] ; then
+  cp $nameFileGeom ./source/src/Geometry.cc
+else
+  echo "Error: The source file '$nameFileGeom' does not exist!"
+  exit 1
+fi
+
+### Clean up work directories
+if [ -d build ] ; then
+  rm -r build
+fi
+
+if [ -d bin ] ;then
+  rm -r bin
+fi
+
+### Build using cmake
+mkdir build
+cd build
+cmake ../source
+make
+make install
